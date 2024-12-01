127d1ceef9fdf920458d252214f4d5a094612f95
Michael Giesler <michael.giesler1@icloud.com>
2024-12-01 13:48:20 -0700

    added rats controller with route handlers

---
+++ src/controllers/RatsController.js
@@ -0,0 +1,39 @@
+import BaseController from "../utils/BaseController";
+import { ratsService } from "../services/RatsService";
+
+export class RatsController extends BaseController {
+  constructor() {
+    super('api/rats')
+    this.router
+      .get('', this.getRats)
+      .get('/:ratId', this.getRatById)
+      .get('/:ratId/missions', this.getRatMissions)
+  }
+
+  async getRats(req, res, next) {
+    try {
+      const rats = await ratsService.getRats()
+      res.send(rats)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getRatById(req, res, next) {
+    try {
+      const rat = await ratsService.getRatById(req.params.ratId)
+      res.send(rat)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getRatMissions(req, res, next) {
+    try {
+      const missions = await ratsService.getMissionsByRatId(req.params.ratId)
+      res.send(missions)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
